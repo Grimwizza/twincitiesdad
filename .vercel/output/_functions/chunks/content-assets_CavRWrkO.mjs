04276dd7fd9a3e95b8c81d8246c35b0430cef765
Grimwizza <ben.luebbert@gmail.com>
2026-02-06 16:43:12 -0600

    build: Generate Vercel deployment output including Astro functions, static assets, and bundled dependencies.

---
+++ .vercel/output/_functions/chunks/content-assets_CavRWrkO.mjs
@@ -0,0 +1,16 @@
+const __ASTRO_IMAGE_IMPORT_2i6oc2 = new Proxy({"src":"/_astro/blog-placeholder-5.Cj9iRoiI.jpg","width":3992,"height":2992,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/benluebbert/Documents/Sites/twincitiesdad/src/assets/blog-placeholder-5.jpg";
+							}
+							
+							return target[name];
+						}
+					});
+
+const contentAssets = new Map([["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fwelcome.md", __ASTRO_IMAGE_IMPORT_2i6oc2]]);
+
+export { contentAssets as default };
