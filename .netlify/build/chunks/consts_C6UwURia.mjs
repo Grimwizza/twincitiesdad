e8948e3b1a4f2531015e775a4d980e593783cae2
Grimwizza <ben.luebbert@gmail.com>
2026-02-07 09:10:40 -0600

    chore: add .netlify to gitignore, remove build artifacts

--- .netlify/build/chunks/consts_C6UwURia.mjs
+++
@@ -1,9 +0,0 @@
-const SITE_TITLE = "Twin Cities Dad";
-const SITE_DESCRIPTION = "Documenting life in Minnesota from a father's perspective. Politics, community, news, and raising a family in the Twin Cities.";
-const SOCIAL_LINKS = {
-  twitter: "https://twitter.com/twincitiesdad",
-  facebook: "https://facebook.com/twincitiesdad",
-  instagram: "https://instagram.com/twincitiesdad"
-};
-
-export { SOCIAL_LINKS as S, SITE_TITLE as a, SITE_DESCRIPTION as b };
